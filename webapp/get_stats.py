ba78bea44dcab9ceeaf5f59d991603a4611d21a2
Sam Robillard <sam.robillard1@gmail.com>
2019-01-11 04:46:42 -0800

    Removed unused test function from get_stats

--- webapp/get_stats.py
+++ webapp/get_stats.py
@@ -1,52 +1,54 @@
 #!/usr/bin/env python3
+import json
 
 import requests
 from requests.auth import HTTPBasicAuth
-import json
 
-class odl_stat_collector(object):
-    
-    
+
+class Odl_Stat_Collector(object):
+
     def __init__(self, controller_ip):
         self.controller_ip = controller_ip
-        self.headers = {'Accept': 'application/json', 'content-type': 'application/json'}
-        #This about addin auth to attributes
+        self.headers = {'Accept': 'application/json',
+                        'content-type': 'application/json'}
+        self.base_url = f"http://{self.controller_ip}:8181/restconf/operational/"
+        # This about addin auth to attributes
         self.topo_data = {}
-    
 
     def get_topo_data(self):
-        url = f"http://{self.controller_ip}:8181/restconf/operational/network-topology:network-topology"
+        url = self.base_url + "network-topology:network-topology"
         topo_request = requests.get(url, headers=self.headers, auth=HTTPBasicAuth("admin", "admin"))
         raw_topo = topo_request.json()
         return raw_topo
-        
+
+    def sort_keys(self, keys):
+
 
     def get_nodes(self, raw_topo):
         nodes = {}
         for node in raw_topo['network-topology']['topology'][0]['node']:
-            #fix for corner case where node does not contain key 'termination point' 
-            links=node.get('termination-point')
-            if(isinstance(links, list)!=True):
+            # fix for corner case where 'termination-point' key does not exist
+            links = node.get('termination-point')
+            if(isinstance(links, list) is not True):
                 continue
 
             nodes[node['node-id']] = {}
             
             for node_int in node['termination-point']:
                 nodes[node['node-id']][node_int['tp-id']] = {}
-        #order nodes from smallest to largest. I.e: openflow:1-openflow:2,etc
-        sortedKeys=sorted(nodes.keys())
-        sortedNodes={}
+        # order nodes from smallest to largest. I.e: openflow:1-openflow:2,etc
+        sortedKeys = sorted(nodes.keys())
+        sortedNodes = {}
         for device in sortedKeys:
-            sortedNodes[device]=nodes[device]
-        
+            sortedNodes[device] = nodes[device]
         return sortedNodes
-        
 
     def get_port_stats(self, node, node_name):
         odl_string = "opendaylight-port-statistics:flow-capable-node-connector-statistics"
-        #Consider putting this in its own moethod and threading
+        # Consider putting this in its own moethod and threading
         for node_int in node:
-            url = f"http://{self.controller_ip}:8181/restconf/operational/opendaylight-inventory:nodes/node/{node_name}/node-connector/{node_int}"
+            
+            url = self.base_url + "opendaylight-inventory:nodes/node/{node_name}/node-connector/{node_int}"
             int_stats_req = requests.get(url, headers=self.headers, auth=HTTPBasicAuth("admin", "admin"))
             raw_int_stats = int_stats_req.json()
             int_stats = raw_int_stats["node-connector"][0][odl_string]
@@ -62,8 +64,7 @@ class odl_stat_collector(object):
             node[node_int]["rx-OverRun-errs"] = int_stats["receive-over-run-error"]
             node[node_int]["rx-CRC-errs"] = int_stats["receive-crc-error"]
         return node
-            
-  
+
     def run(self):
         topo = self.get_topo_data()
         nodes = self.get_nodes(topo)
@@ -72,16 +73,4 @@ class odl_stat_collector(object):
                 nodes[node] = self.get_port_stats(nodes[node], node)
         return nodes
 
-        
-def odl_testing():
-    o = odl_stat_collector("192.168.0.17")
-    topo = o.get_topo_data()
-    nodes = o.get_nodes(topo)
-    for node in nodes:
-        if "host" not in node:
-            #print(node)
-            nodes[node] = o.get_port_stats(nodes[node], node)
-        print(node, "{")
-        print(nodes[node])
-        print("}")
-        
+
