c26abfd81c2d37d1eede8f0d5c9d40757215224b
Brad FItzgerald <brad.fitzgerald96@gmail.com>
2019-01-16 00:02:59 -0500

    merger commit

--- webapp/app.py
+++ webapp/app.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-import getip
+import auxiliary
 from flask import Flask, render_template
 from generatetopo import odl_topo_builder
 from get_stats import Odl_Stat_Collector
 
 app = Flask(__name__)
 
-# odlControllerList = getip.findController()
+# odlControllerList = auxiliary.device_scan()
 # controllerIP = odlControllerList[0]
 controllerIP = "134.117.89.138"
 
@@ -16,16 +16,18 @@ controllerIP = "134.117.89.138"
 def index():
     return render_template('home.html')
 
+
 @app.route("/topology")
 def topology():
     parser = odl_topo_builder(controllerIP)
     return render_template('topo.html', topologyInfo=parser.fetch_topology())
 
+
 @app.route("/node-stats")
 def node_stats():
     o = Odl_Stat_Collector(controllerIP)
     return render_template('nodes.html', nodes=o.run())
-         
+       
 @app.route("/controller")
 def getControllerIP():
     # print(odlControllerList)
