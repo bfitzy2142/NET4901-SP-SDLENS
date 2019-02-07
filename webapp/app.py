7501739a8e816fa037d7454b5eb4da0f75a22588
Sam Robillard <samuelrobillard@cmail.carleton.ca>
2019-02-06 17:30:35 -0800

    Added first iteration of user login stuff

--- webapp/app.py
+++ webapp/app.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+from functools import wraps
+
 from generatetopo import odl_topo_builder
 from get_stats import Odl_Stat_Collector
 from deviceInfo import odl_switch_info
@@ -13,34 +18,142 @@ app = Flask(__name__)
 # controllerIP = odlControllerList[0]
 controllerIP = "134.117.89.138"
 
+# config mysql
+app.secret_key = "secret123"
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'sdlens'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+# Init Mysql
+mysql = MySQL(app)
+
 
 @app.route("/")
 def index():
+    if 'logged_in' in session:
+        return render_template('home.html')
+    else:
+        return redirect(url_for('login'))
+
+
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+
+@app.route("/dashboard")
+@is_logged_in
+def dashboard():
     return render_template('home.html')
 
 
 @app.route("/topology")
+@is_logged_in
 def topology():
     parser = odl_topo_builder(controllerIP)
     return render_template('topo.html', topologyInfo=parser.fetch_topology())
 
 
 @app.route("/node-stats")
+@is_logged_in
 def node_stats():
     o = Odl_Stat_Collector(controllerIP)
     return render_template('nodes.html', nodes=o.run())
 
 
 @app.route("/device-info")
+@is_logged_in
 def device_info():
     o = odl_switch_info(controllerIP)
     return render_template('deviceInfo.html', nodes=o.run())
 
  
 @app.route("/controller")
+@is_logged_in
 def getControllerIP():
     # print(odlControllerList)
     return render_template('settings.html', odlIP=controllerIP)
+
+
+class RegisterFrom(Form):
+    name = StringField('Name', [validators.Length(min=1, max=100)])
+    username = StringField('Username', [validators.Length(min=4, max=20)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterFrom(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+        # create cursor
+        cur = mysql.connection.cursor()
+        query = ("INSERT INTO users(name, email, username, password) " +
+                 f"VALUES('{name}', '{email}', '{username}', '{password}')")
+        print(query)
+        cur.execute(query)
+        mysql.connection.commit()
+        cur.close()
+        flash('You are now registered and can login', 'success')
+        redirect(url_for('index'))
+
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get Form Fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+        # Get user by username
+        result = cur.execute(f"SELECT * FROM users WHERE username = '{username}'")
+
+        if result > 0:
+            # Get stored hash
+            data = cur.fetchone()
+            password = data['password']
+
+            # Compare passwords
+            if sha256_crypt.verify(password_candidate, password):
+                session['logged_in'] = True
+                session['username'] = username
+                flash('You are now logged in', 'sucess')
+                return redirect(url_for('dashboard'))
+            else:
+                error = 'invalid login'
+                return render_template('login.html', error=error)
+            cur.close()
+        else:
+            error = 'Username not found'
+            return render_template('login.html', error=error)
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@is_logged_in
+def logout():
+    session.clear()
+    flash('You are now logged out')
+    return redirect(url_for('login'))
 
 
 if __name__ == "__main__":
