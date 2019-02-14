7c7e8d9798542fbc33c310bd5b54dde6ce796824
Sam Cook <sam@dhcp-32-115.cu-wireless-faculty.carleton.ca>
2019-02-14 14:58:00 -0500

    Changed graphs to show diffence in counter

--- webapp/gen_graphs.py
+++ webapp/gen_graphs.py
@@ -22,7 +22,8 @@ class sql_graph_info(object):
         Pulls the port counters and timestamp from each table in the database.
         """
         query = (
-            f"SELECT timestamp, Rx_pckts, Tx_pckts, Rx_drops, Tx_drops FROM {node}_counters WHERE "
+            f"SELECT timestamp, Rx_pckts, Tx_pckts, Rx_drops, Tx_drops "
+            f"FROM {node}_counters WHERE "
             f"Interface='{interface}'"
         )
 
@@ -35,8 +36,10 @@ class sql_graph_info(object):
         cur = mydb.cursor()
         cur.execute(query)
         response = cur.fetchall()
-        
+
         graphPoints = []
+        displayPoints = []
+        dataPointDict = {}
 
         for dataPoint in response:
             date = str(dataPoint[0])
@@ -45,8 +48,25 @@ class sql_graph_info(object):
             rx_drops = dataPoint[3]
             tx_drops = dataPoint[4]
 
-            dataPointDict = {"date" : date, "rx_count" : rx_count, "tx_count" : tx_count, "rx_drops" : rx_drops, "tx_drops" : tx_drops}
-            graphPoints.append(dataPointDict)
+            if dataPointDict:
+                old_rx_c = dataPointDict['rx_count']
+                old_tx_c = dataPointDict["tx_count"]
+                old_rx_d = dataPointDict["rx_drops"]
+                old_tx_d = dataPointDict["tx_drops"]
 
-        return graphPoints
-        
+                dif_rx_c = rx_count - old_rx_c
+                dif_tx_c = tx_count - old_tx_c
+                dif_rx_d = rx_drops - old_rx_d
+                dif_tx_d = tx_drops - old_tx_d
+
+                difDict = {"date": date, "rx_count": dif_rx_c,
+                           "tx_count": dif_tx_c,
+                           "rx_drops": dif_rx_d,
+                           "tx_drops": dif_tx_d}
+                displayPoints.append(difDict)
+
+            dataPointDict = {"date": date, "rx_count": rx_count,
+                             "tx_count": tx_count, "rx_drops": rx_drops,
+                             "tx_drops": tx_drops}
+            graphPoints.append(dataPointDict)
+        return displayPoints
